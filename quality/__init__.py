b9a545c80d5b3a352dfee67b2582eeb187f7ba5e
Bob Borges <robert.d.borges@gmail.com>
2024-11-15 22:27:28 +0100

    docs: update docs

--- quality/__init__.py
+++ quality/__init__.py
@@ -8,4 +8,12 @@
 .. include:: docs/qe_ocr-estimation.md
     :start-after: Summary
     :end-before: What is the problem
+
+### Speaker Mapping Accuracy -- `qe_speaker-mapping.py`
+
+.. include:: docs/qe_speaker-mapping.md
+    :start-after: Summary
+    :end-before: What is the problem
+
+
 """
