b1eec3a069f944ab58890f5b9d6f0db8a9f01cfa
ninpnin <vainoyrjanainen@icloud.com>
2025-05-06 15:48:45 +0200

    feat: fuzzy matching for note/seg quality estimation

--- quality/qe_segment-classification.py
+++ quality/qe_segment-classification.py
@@ -12,6 +12,8 @@ from pyriksdagen.utils import (
     elem_iter,
     infer_metadata,
     parse_tei,
+    XML_NS,
+    TEI_NS
 #    pathize_protocol_id,
 )
 from scipy.stats import beta
@@ -19,9 +21,9 @@ from tqdm import tqdm
 import os
 import pandas as pd
 import re
-
-
-
+from pathlib import Path
+import warnings
+import editdistance
 
 # rm after fn released in pyriksdagen
 def pathize_protocol_id(protocol_id):
@@ -81,47 +83,122 @@ def match_elem(elem, df, ns):
         return 0,1
 
 
+def fuzzy_equality(t1, t2):
+    equal = True
+    if len(t1) * 0.91 <= len(t2) and len(t1) * 1.09 >= len(t2):
+        dist = editdistance.eval(t1, t2)
+        if dist / min(len(t1), len(t2)) > 0.15:
+            equal = False
+        if dist / min(len(t1), len(t2)) > 0.1 and min(len(t1), len(t2)) > 20:
+            equal = False
+        if dist >= 13:
+            equal = False
+    else:
+        equal = False
+    return equal
+
 # Fix parallellization
-def estimate_accuracy(protocol, df):
+def estimate_accuracy(protocol, df, use_ids=True):
     """
     Return correct / incorrect counts of (mis)matched segments.
     """
     root, ns = parse_tei(protocol)
     correct, incorrect = 0, 0
     ids = set(df["elem_id"])
+    texts = set(df["text"])
+    #print(texts)
+
+    textmap = {t: i for t,i in zip(df["text"], df["elem_id"])}
+    #exit()
     for tag, elem in elem_iter(root):
         if tag == "u":
             x = None
             for subelem in elem:
                 x = subelem.attrib.get(f'{ns["xml_ns"]}id', None)
-                if x in ids:
+                subelem_text = None
+                if subelem.text is not None:
                     subelem_text = " ".join(subelem.text.split())
+                if x in ids and use_ids:
                     results = match_elem(subelem, df, ns)
                     correct += results[0]
                     incorrect += results[1]
 
+                elif subelem_text in texts:
+                    newid = textmap[subelem_text]
+                    subelem.attrib[f'{XML_NS}id'] = newid
+                    results = match_elem(subelem, df, ns)
+                    correct += results[0]
+                    incorrect += results[1]
+
+                    texts.remove(subelem_text)
+
+                elif subelem_text is not None and not use_ids:
+                    for t in list(texts):
+                        if fuzzy_equality(t, subelem_text):
+                            newid = textmap[t]
+                            subelem.attrib[f'{XML_NS}id'] = newid
+                            results = match_elem(subelem, df, ns)
+                            correct += results[0]
+                            incorrect += results[1]
+                            texts.remove(t)
+
         elif tag in ["note"]:
             x = elem.attrib.get(f'{ns["xml_ns"]}id', None)
-            if x in ids:
+            elem_text = None
+            if elem.text is not None:
                 elem_text = " ".join(elem.text.split())
+            if x in ids and use_ids:
+                results = match_elem(elem, df, ns)
+                correct += results[0]
+                incorrect += results[1]
+            elif elem_text in texts:
+                newid = textmap[elem_text]
+                elem.attrib[f'{XML_NS}id'] = newid
                 results = match_elem(elem, df, ns)
                 correct += results[0]
                 incorrect += results[1]
 
+                texts.remove(elem_text)
+            elif elem_text is not None and not use_ids:
+                for t in list(texts):
+                    if fuzzy_equality(t, elem_text):
+                        newid = textmap[t]
+                        elem.attrib[f'{XML_NS}id'] = newid
+                        results = match_elem(elem, df, ns)
+                        correct += results[0]
+                        incorrect += results[1]
+                        texts.remove(t)
+
+    if correct + incorrect == 0:
+        warnings.warn(f"No IDs matched in: {protocol}")
     return correct, incorrect
 
+def get_old_id(record):
+    record_id = Path(record).stem.split(".xml")[0]
+    record_start = "-".join(record_id.split("-")[:-1])
+    try:
+        record_number = int(record_id.split("-")[-1])
+        return f"{record_start}-{record_number}"
+    except Exception:
+        warnings.warn(f"Whoops: {record}")
 
 def main(args):
     rows = []
     correct, incorrect = 0, 0
     df = pd.read_csv(args.annotated_data)
-    df["protocol_id"] = df["protocol_id"].apply(lambda x: pathize_protocol_id(x))
-    records = list(df["protocol_id"].unique())
+    df["protocol_id"] = df["protocol_id"]#.apply(lambda x: pathize_protocol_id(x))
+    #records = list(df["protocol_id"].unique())
+    print(df)
+    print("USE FUZZY MATCHING", args.fuzzy)
+    records = args.records
     for record in tqdm(records):
-        df_p = df[df["protocol_id"] == record]
+        record_id = get_old_id(record)
+        df_p = df[df["protocol_id"] == record_id]
         if len(df_p) >= 1:
+            #print(df_p)
+            #exit()
             metadata = infer_metadata(record)
-            acc = estimate_accuracy(record, df_p)
+            acc = estimate_accuracy(record, df_p, use_ids=not args.fuzzy)
             correct += acc[0]
             incorrect += acc[1]
             if acc[1] + acc[0] > 0:
@@ -161,4 +238,10 @@ if __name__ == '__main__':
                         type=str,
                         default="quality/estimates/segment-classification",
                         help="Path where the current estimate will be written")
-    main(impute_args(parser.parse_args()))
+    parser.add_argument("--fuzzy",
+                        type=bool,
+                        default=False,
+                        help="Whether to use fuzzy text matching instead of IDs")
+    args = impute_args(parser.parse_args())
+    #print(args)
+    main(args)
