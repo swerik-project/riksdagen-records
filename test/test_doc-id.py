7233da3c78ea467111eb7564781b18beb43e2ba1
Bob Borges <robert.d.borges@gmail.com>
2025-08-13 11:27:21 +0200

    feat: actually fail the test when necessary

--- test/test_doc-id.py
+++ test/test_doc-id.py
@@ -39,9 +39,11 @@ class DocIdTests(unittest.TestCase):
         print("\nDocs with no ID:", cls.no_id[:5], "\n", len(cls.no_id), "instances")
         if len(cls.no_id) > 0:
             pd.DataFrame(cls.no_id, columns=["record", "problem"]).to_csv("test/results/doc-wout-id.tsv", sep="\t", index=False)
+
         print("\nDocs with ID / FN mismatch", cls.id_not_fn[:5], "\n", len(cls.id_not_fn), "instances")
         if len(cls.id_not_fn) > 0:
             pd.DataFrame(cls.id_not_fn, columns=["record", "ID", "filename"]).to_csv("test/results/id-not-filename.tsv", sep="\t", index=False)
+
         print("\nDocs with ID / FACS mismatch", cls.id_facs_mismatch[:5], "\n", len(cls.id_facs_mismatch), "instances")
         if len(cls.id_facs_mismatch) > 0:
             pd.DataFrame(cls.id_facs_mismatch, columns=["record", "facs",  "problem"]).to_csv("test/results/id-not-facs.tsv", sep="\t", index=False)
@@ -55,6 +57,7 @@ class DocIdTests(unittest.TestCase):
             root, ns = parse_tei(record)
             if f"{ns['xml_ns']}id" not in root.attrib:
                 self.no_id.append([record, "no ID attrib"])
+        self.assertEqual(0, len(self.no_id), "Some docs have no ID")
 
 
     def test_doc_id_is_filename(self):
@@ -70,6 +73,7 @@ class DocIdTests(unittest.TestCase):
                 FN = record.split('/')[-1][:-4]
                 if not ID == FN:
                     self.id_not_fn.append([record, ID, FN])
+        self.assertEqual(0, len(self.id_not_fn), "Some docs IDs don't match their FN")
 
 
     def test_doc_id_in_dacs(self):
@@ -93,10 +97,11 @@ class DocIdTests(unittest.TestCase):
                         doc = s[-2]
                         if doc != ID:
                             self.id_facs_mismatch.append([record, facs, "facs not ID"])
-                        else:
-                            print("facs problem 1")
                         if "_".join(page.split("_")[:-1]) != ID:
                             self.id_facs_mismatch.append([record, facs, "facs not page"])
+
+        self.assertEqual(0, len(self.id_facs_mismatch), "Some docs' IDs don't match the URL in their pb elem facs attribute")
+
 
 
 
