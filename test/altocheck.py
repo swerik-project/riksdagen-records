283bee71dd76ea75250c97043264f949cd029f81
ninpnin <vainoyrjanainen@icloud.com>
2026-02-25 13:44:52 +0200

    fix: make ALTO test not crash (still todos left)

--- test/altocheck.py
+++ test/altocheck.py
@@ -6,6 +6,10 @@ from nltk import edit_distance
 import requests
 import random
 from pathlib import Path
+import warnings
+from trainerlog import get_logger
+
+LOGGER = get_logger("ALTO-test")
 
 parser = etree.XMLParser(remove_blank_text=True)
 def get_root(path):
@@ -18,7 +22,7 @@ def get_alto_words(content):
         altofile = alto.parse(content)
         words = altofile.extract_words()
     except:
-        print("ALTO XML parsing unsuccessful")
+        LOGGER.error("ALTO XML parsing unsuccessful")
         return None
     return words
 
@@ -26,10 +30,12 @@ def get_pc_words(root, page="random", ns="{http://www.tei-c.org/ns/1.0}"):
     if page == "random":
         pbs = list(root.findall(f".//{ns}pb"))
         pb_sources = [pb.attrib["facs"] for pb in pbs]
-        page_strs = [page.split(".jp2")[0][-3:] for page in pb_sources]
-        pages = [int(page_str) for page_str in page_strs]
-        page = random.choice(pages)
-        print("PAGE:", page)
+        page_link = random.choice(pb_sources)
+        filename = page_link.split("/")[-1].split(".")[0]
+        page = filename[-3:]
+        LOGGER.info(f"Randomly selected page {page}")
+        if not page.isdigit():
+            LOGGER.warning(f"Page link does not end with a numeric index {page_link}")
 
     words = []
     correct_page = False
@@ -38,10 +44,11 @@ def get_pc_words(root, page="random", ns="{http://www.tei-c.org/ns/1.0}"):
         for div in root.findall(f".//{ns}div"):
             for elem in div:
                 if elem.tag == f"{ns}pb":
-                    page_no = f"{page:03d}.jp2"
-                    correct_page = page_no in str(elem.attrib["facs"])
+                    correct_page = page_link == elem.attrib["facs"]
                     if correct_page:
-                        alto_url = elem.attrib["facs"].replace(".jp2/_view", ".xml")
+                        # TODO: get alto URL or PDF url and tesseract
+                        alto_url = elem.attrib["facs"]
+                        LOGGER.info(f"ALTO URL: {alto_url}")
                 if correct_page:
                     clean_string = ' '.join([n.strip() for n in elem.itertext()]).strip()
                     words += clean_string.split()
@@ -65,7 +72,7 @@ def calculate_difference(root_pc, page="random", auth=None):
     words_pc, alto_url, page = get_pc_words(root_pc, page=page)
     r = requests.get(alto_url, auth=auth)
     if r.content is None:
-        print("HTTP response has no content")
+        warnings.warn("HTTP response has no content")
         return None#0, 0.0, 0
     words_alto = get_alto_words(r.content)
     if words_alto is None:
@@ -90,23 +97,24 @@ class Test(unittest.TestCase):
     # Parla-clarin generated from example OCR XML
     @unittest.skip()
     def test_protocols(self):
+        PAGES_PER_DECADE = 100
         folder = "data/"
         p = Path(folder)
         auth = os.environ.get("KBLAB_USERNAME"), os.environ.get("KBLAB_PASSWORD")
 
         all_testcases = []
         decades = list(range(1860, 1990, 10))
-        print(f"Testing decades: {decades}")
+        LOGGER.info(f"Testing decades: {decades}")
         for decade in decades:
             testcases = list(p.glob(f"{decade // 10}*/*.xml"))
             testcases = sorted(testcases, key=lambda v: random.random())
-            all_testcases = all_testcases + sorted(testcases[:100])
+            all_testcases = all_testcases + sorted(testcases[:PAGES_PER_DECADE])
         
         percentage_fail = []
         absolute_fail = []
         loading_fail = []
         for protocol_path in all_testcases:
-            print(protocol_path)
+            LOGGER.info(f"Testing protocol {protocol_path}")
             protocol_id = str(protocol_path.stem)
             root_pc = get_root(protocol_path.relative_to("."))
             difference = calculate_difference(root_pc, auth=auth)
@@ -114,19 +122,19 @@ class Test(unittest.TestCase):
                 loading_fail.append(protocol_id)
             else:
                 absolute, percentage, page = difference
-                print(f"{absolute} errors, {percentage * 100:.1f}%")
+                LOGGER.info(f"{absolute} errors, {percentage * 100:.1f}%")
                 if absolute >= 3:
-                    print(f"{protocol_id}: {page} (absolute) over limit")
+                    LOGGER.warning(f"{protocol_id}: {page} (absolute) over limit")
                     absolute_fail.append(protocol_id)
                 if percentage >= 0.05:
-                    print(f"{protocol_id}: {page} (percentage) over limit")
+                    LOGGER.warning(f"{protocol_id}: {page} (percentage) over limit")
                     percentage_fail.append(protocol_id)
         
         absolute_fail_ratio = len(absolute_fail) / len(all_testcases)
         percentage_fail_ratio = len(percentage_fail) / len(all_testcases)
         succesful_loading_ratio = len(loading_fail) / len(all_testcases)
-        print(f"Proportion of protocols with over 3 mismatching sentences: {absolute_fail_ratio}")
-        print(f"Proportion of protocols with over 0.05% mismatching sentences: {percentage_fail_ratio}")
+        LOGGER.info(f"Proportion of protocols with over 3 mismatching sentences: {absolute_fail_ratio}")
+        LOGGER.info(f"Proportion of protocols with over 0.05% mismatching sentences: {percentage_fail_ratio}")
         self.assertTrue(absolute_fail_ratio < 0.03, f"Absolute ratio {absolute_fail_ratio} too high {absolute_fail}")
         self.assertTrue(percentage_fail_ratio < 0.05, f"Percentage ratio {percentage_fail_ratio} too high {percentage_fail}")
         self.assertTrue(succesful_loading_ratio < 0.03, f"Loading ratio {succesful_loading_ratio} too high {loading_fail}")
