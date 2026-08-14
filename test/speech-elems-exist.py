fbf180c5e60de53cd7ae246787542d5f346b0071
ninpnin <vainoyrjanainen@icloud.com>
2026-08-14 10:58:57 +0300

    fix: add missing assert in the test

--- test/speech-elems-exist.py
+++ test/speech-elems-exist.py
@@ -19,25 +19,10 @@ class Test(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.records = sorted(glob("data/*/*.xml"))
-        cls.phantom_speech_elements = {}
-
-
-    @classmethod
-    def tearDownClass(cls):
-        if len(cls.phantom_speech_elements) > 0:
-            with open("test/results/phantom_speech_elements.json", "w+") as outf:
-                json.dump(cls.phantom_speech_elements, outf, ensure_ascii=False, indent=4)
-            logger.warn("Not all speech elements are accounted for. Test fails")
-        try:
-            assert len(cls.phantom_speech_elements) == 0
-            logger.info("Test passes.")
-        except:
-            logger.critical("Test fails")
-
-
-
 
     def test_not_phantom_speech_elems(self):
+        phantom_speech_elements = {}
+
         no_records = len(self.records)
         records_w_no_speeches = 0
         for record in tqdm(self.records):
@@ -60,14 +45,26 @@ class Test(unittest.TestCase):
                     if not root.xpath(
                             f"boolean(.//tei:u[@xml:id=\"{se}\"])",
                             namespaces={"tei": ns["tei_ns"][1:-1], "xml": ns["xml_ns"][1:-1]}):
-                        if record not in self.phantom_speech_elements:
-                            self.phantom_speech_elements[record] = []
-                        self.phantom_speech_elements[record].append(se)
-                        logger.error(f"{record} : {se} not found")
-                        #logger.warn(ns)
+                        if record not in phantom_speech_elements:
+                            phantom_speech_elements[record] = []
+                        phantom_speech_elements[record].append(se)
+
+            if record in phantom_speech_elements:
+                missing_elems = phantom_speech_elements[record]
+                missing_elems = ", ".join(missing_elems)
+                if len(missing_elems) > 300:
+                    missing_elems = missing_elems[:200] + " [...] " + missing_elems[-50:]
+                logger.error(f"{record} : the following elements: {missing_elems} not found")
 
             self.assertGreaterEqual(no_records * 0.2, records_w_no_speeches, f"Less than 20% of records should have no speeches ({records_w_no_speeches} found)")
 
+        if len(phantom_speech_elements) > 0:
+            with open("test/results/phantom_speech_elements.json", "w+") as outf:
+                json.dump(phantom_speech_elements, outf, ensure_ascii=False, indent=4)
+            logger.warn("Not all speech elements are accounted for. Test fails")
+        
+        no_errors = len(phantom_speech_elements)
+        self.assertEqual(0, no_errors, f"Phantom speech elements detected in {no_errors} records")
 
 
 
